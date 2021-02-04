be5ec3e8a93c63ab7749e0262bdce2c33038c885
Yulia <32438782+Coriolan8@users.noreply.github.com>
2021-02-04 21:00:08 +0300

    Delete folder, move files
    Delete folder with unuseful files, move files in main folder.

--- add file.py
+++ add file.py
@@ -14,7 +14,7 @@ try:
     email = browser.find_element_by_name("email")
     email.send_keys("ds@fg.c")
     current_dir = os.path.abspath(os.path.dirname(__file__))    # получаем путь к директории текущего исполняемого файла
-    file_path = os.path.join(current_dir, '../file.txt')           # добавляем к этому пути имя файла
+    file_path = os.path.join(current_dir, 'file.txt')           # добавляем к этому пути имя файла
     browser.find_element_by_name('file').send_keys(file_path)
     submit = browser.find_element_by_xpath("//button[text()= 'Submit']")
     submit.click()
